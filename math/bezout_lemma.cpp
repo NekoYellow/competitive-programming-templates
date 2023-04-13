ba85d5fae5d21eeae7c8a7f4ddf69471a360efc5
Yueyuan Huang <90030811+NekoYellow@users.noreply.github.com>
2023-04-14 00:42:20 +0800

    Create bezout_lemma.cpp

---
+++ math/bezout_lemma.cpp
@@ -0,0 +1,28 @@
+/* Codeforces 510D */
+/* Bezout's Lemma + Bag DP */
+/* Bezout's Lemma: for integers a,b (ab!=0) exist x,y so that ax+by=gcd(a,b). */
+/* => when gcd(a,b)=1, exist x,y : ax+by=1. */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> l(n), c(n);
+    for (int i = 0; i < n; i++) cin >> l[i];
+    for (int i = 0; i < n; i++) cin >> c[i];
+    map<int, int> f; // less space compared with array
+    f[0] = 0; // start point; gcd(0, x) = x
+    for (int i = 0; i < n; i++) {
+        for (auto [pred, cost]: f) { // iterate prev-d from small to large
+            int d = gcd(pred, l[i]);
+            if (!f.count(d)) f[d] = cost + c[i];
+            else f[d] = min(f[d], cost + c[i]);
+        }
+    }
+    if(!f.count(1)) f[1] = -1;
+    cout << f[1] << '\n';
+    return 0;
+}
