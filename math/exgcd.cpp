0db687f23caec6332cd9fd3ac377fb04a72841eb
Yueyuan Huang <90030811+NekoYellow@users.noreply.github.com>
2023-04-15 22:07:46 +0800

    Create exgcd.cpp

---
+++ math/exgcd.cpp
@@ -0,0 +1,22 @@
+/* LOJ$2605 solve ax = 1 (mod b) */
+#include <bits/stdc++.h>
+using namespace std;
+
+void exgcd(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1, y = 0;
+        return;
+    }
+    exgcd(b, a % b, y, x);
+    y -= a / b * x;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int x, y;
+    exgcd(a, b, x, y);
+    x = (x % b + b) % b;
+    cout << x << '\n';
+    return 0;
+}
