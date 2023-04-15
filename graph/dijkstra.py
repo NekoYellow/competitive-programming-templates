8c96c6d5b2cdfddb38b1cb068b05fb343f1fdd98
Yueyuan Huang <90030811+NekoYellow@users.noreply.github.com>
2023-04-16 00:47:21 +0800

    Update dijkstra.py
    Substitute adjacency matrix with adjacency table

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -1,19 +1,21 @@
+"LC 6336"
 from math import inf
 from heapq import heappush, heappop
-from typing import List
 
 class Graph:
-    def __init__(self, n: int, edges: List[List[int]]):
+    def __init__(self, n, edges):
         self.n = n
-        self.G = [[0]*n for _ in range(n)]
+        self.G = {}
         for edge in edges:
             self.addEdge(edge)
 
-    def addEdge(self, edge: List[int]) -> None:
+    def addEdge(self, edge):
         u, v, w = edge
-        self.G[u][v] = w
+        if u not in self.G:
+            self.G[u] = set()
+        self.G[u].add((v, w))
 
-    def dijkstra(self, src: int, dst: int) -> int:
+    def shortestPath(self, src, dst):
         "Return distance of the shortest path from `src` to `dst`."
         dist = [inf] * self.n # dist[u]:= distance of the shortest path from src to u
         dist[src] = 0
@@ -22,9 +24,8 @@ class Graph:
             du, u = heappop(pq) # u:= vertex with the min dist (du) in tree
             if du > dist[u]: continue
             if u == dst: return du
-            for v in range(self.n):
-                if not self.G[u][v]: continue # no edge from u to v
-                if dist[u] + self.G[u][v] < dist[v]: # shorter path found
-                    dist[v] = dist[u] + self.G[u][v]
+            for v, w in self.G.get(u, []):
+                if dist[u] + w < dist[v]: # shorter path found
+                    dist[v] = dist[u] + w
                     heappush(pq, (dist[v], v)) # v is now in tree
         return dist[dst] if dist[dst] != inf else -1
