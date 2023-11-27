f1c718d31b7f51d7c19aee006332657098959717
Yueyuan Huang <harvyhyy@gmail.com>
2023-11-27 19:51:57 +0100

    Update maxflow:  EK -> Dinic; Rename aca -> acm

--- graph/maxflow.cpp
+++ graph/maxflow.cpp
@@ -1,62 +1,93 @@
-/* Edmonds-Karp : O(VE^2) */
-/* Cannot remember the origin problem, so the correctness is unknown */
+/* Luogu P3376 */
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const char nl = '\n';
 
-const int INF = 1e9, MAXN = 300;
-int n, m, graph[MAXN][MAXN], pre[MAXN]; // graph acts also as residual network
+class MF {
+public:
+    MF(int _n, int _s, int _t) {
+        n = _n; s = _s; t = _t;
+        head.resize(n);
+        fill(head.begin(), head.end(), -1);
+        dep.resize(n);
+    }
 
-int bfs(int s, int t) { // find enlarging path : O(VE)
-    int flow[MAXN];
-    memset(pre, -1, sizeof(pre));
-    flow[s] = INF, pre[s] = 0; // init start vertex
-    queue<int> q;
-    q.push(s);
-    while (!q.empty()) {
-        int u = q.front(); q.pop();
-        if (u == t) break; // path found, stop bfs
-        for (int i = 1; i <= m; i++) { // bfs every vertex
-            if (i != s && graph[u][i] > 0 && pre[i] != -1) {
-                pre[i] = u; // record path
-                q.push(i);
-                flow[i] = min(flow[u], graph[u][i]); // update vertex flow(flux)
+    void addedge(int u, int v, int w) {
+        e.push_back({v, head[u], w, 0});
+        head[u] = e.size()-1;
+        e.push_back({u, head[v], 0, 0});
+        head[v] = e.size()-1;
+    }
+
+    ll dinic() {
+        ll maxflow = 0;
+        int inf = (1 << 31) - 1;
+        while (bfs()) {
+            cur = head;
+            maxflow += dfs(s, inf);
+        }
+        return maxflow;
+    }
+
+private:
+    int n; // |V|
+    int s, t; // source, sink
+    struct Edge {
+        int v, nxt, c, f; // to, next edge, capacity, flow
+    };
+    vector<Edge> e;
+    vector<int> head; // forward star repr
+    vector<int> dep, cur; // for dfs
+
+    bool bfs() {
+        queue<int> q;
+        fill(dep.begin(), dep.end(), 0);
+        dep[s] = 1;
+        q.push(s);
+        while (q.size()) {
+            int u = q.front(); q.pop();
+            for (int i = head[u]; ~i; i = e[i].nxt) {
+                int v = e[i].v;
+                if (!dep[v] && e[i].c > e[i].f) {
+                    dep[v] = dep[u]+1;
+                    q.push(v);
+                }
             }
         }
+        return dep[t]; // if t is reachable from s
     }
-    if (pre[t] == -1) return -1; // have not found new enlarging path
-    return flow[t]; // return flux of this enlarging path
-}
 
-int maxflow(int s, int t) {
-    int mf = 0;
-    while (1) {
-        int flow = bfs(s, t); // find a path and return its flux
-        if (flow == -1) break; // no new path
-        int cur = t;
-        while (cur != s) { // update residual network along the path
-            int par = pre[cur];
-            graph[par][cur] -= flow; // decrease on the direction
-            graph[cur][par] += flow; // increase on the opposite dir
-            cur = par;
+    int dfs(int u, int flow) {
+        if (u == t || !flow) return flow;
+        int ret = 0;
+        for (int &i = cur[u]; ~i; i = e[i].nxt) {
+            int v = e[i].v;
+            if (dep[v] == dep[u]+1) {
+                int d = dfs(v, min(flow-ret, e[i].c-e[i].f));
+                if (!d) continue;
+                e[i].f += d, e[i^1].f -= d;
+                ret += d;
+                if (ret == flow) break;
+            }
         }
-        mf += flow;
+        return ret;
     }
-    return mf;
-}
+};
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        memset(graph, 0, sizeof(graph));
-        for (int i = 0; i < n; i++) {
-            int u, v, w;
-            cin >> u >> v >> w;
-            graph[u][v] += w; // duplicate edges may exist
-        }
-        cout << maxflow(1, m) << endl;
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int n, m, s, t;
+    cin >> n >> m >> s >> t;
+    MF mf(n, s-1, t-1);
+    while (m--) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        mf.addedge(u-1, v-1, w);
     }
+    cout << mf.dinic() << nl;
+
     return 0;
 }
---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const char nl = '\n';
+
+void solve() {
+    
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
