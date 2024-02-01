5bd26b4167cadcccdd5403aede6937d6d2bf96c9
Yueyuan Huang <harvyhyy@gmail.com>
2024-02-01 20:02:40 +0800

    Rename math/ring.cpp -> math/comb.cpp

--- math/comb.cpp
+++ math/comb.cpp
@@ -40,49 +40,46 @@ struct MInt {
     friend constexpr bool operator!=(const MInt& lhs, const MInt& rhs) { return lhs.val() != rhs.val(); }
 };
 
-constexpr int P = 1000000007;
+/* TO FILL IN */
+constexpr int P = 1E9 + 7;
 using Z = MInt<P>;
 
-
 struct Comb {
-    int n;
+    int _n;
     std::vector<Z> _fac;
     std::vector<Z> _invfac;
     std::vector<Z> _inv;
     
-    Comb() : n{0}, _fac{1}, _invfac{1}, _inv{0} {}
-    Comb(int n) : Comb() {
-        init(n);
-    }
+    Comb() : _n{0}, _fac{1}, _invfac{1}, _inv{0} {}
+    Comb(int n) : Comb() { init(n); }
     
     void init(int m) {
         m = std::min(m, Z::getMod() - 1);
-        if (m <= n) return;
+        if (m <= _n) return;
         _fac.resize(m + 1);
         _invfac.resize(m + 1);
         _inv.resize(m + 1);
-        
-        for (int i = n + 1; i <= m; i++) {
+        for (int i = _n + 1; i <= m; i++) {
             _fac[i] = _fac[i - 1] * i;
         }
         _invfac[m] = _fac[m].inv();
-        for (int i = m; i > n; i--) {
+        for (int i = m; i > _n; i--) {
             _invfac[i - 1] = _invfac[i] * i;
             _inv[i] = _invfac[i] * _fac[i - 1];
         }
-        n = m;
+        _n = m;
     }
     
     Z fac(int m) {
-        if (m > n) init(2 * m);
+        if (m > _n) init(2 * m);
         return _fac[m];
     }
     Z invfac(int m) {
-        if (m > n) init(2 * m);
+        if (m > _n) init(2 * m);
         return _invfac[m];
     }
     Z inv(int m) {
-        if (m > n) init(2 * m);
+        if (m > _n) init(2 * m);
         return _inv[m];
     }
     Z binom(int n, int m) {
