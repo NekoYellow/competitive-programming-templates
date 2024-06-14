a42442e2df9e75a143c46f494f8df4cb92c124f4
Yueyuan Huang <harvyhyy@gmail.com>
2024-06-14 17:42:18 +0800

    Modify dsu

--- datastruct/dsu.cpp
+++ datastruct/dsu.cpp
@@ -3,17 +3,29 @@
 using namespace std;
 
 // struct DSU {
-//     vector<int> par;
+//     vector<int> par, sz;
 //     DSU(int n) {
-//         par.resize(n);
-//         iota(par.begin(), par.end(), 0);
+//         par.resize(n); iota(par.begin(), par.end(), 0);
+//         sz.assign(n, 1);
 //     }
 //     int find(int x) {
 //         if (x == par[x]) return x;
 //         return par[x] = find(par[x]);
 //     }
+//     int size(int x) {
+//         return sz[find(x)];
+//     }
 //     void merge(int x, int y) {
-//         par[find(y)] = find(x);
+//         int rx = find(x), ry = find(y);
+//         if (rx != ry) {
+//             if (sz[rx] < sz[ry]) {
+//                 par[rx] = ry;
+//                 sz[ry] += sz[rx];
+//             } else {
+//                 par[ry] = rx;
+//                 sz[rx] += sz[ry];
+//             }
+//         }
 //     }
 // };
 
@@ -28,8 +40,7 @@ struct DSU {
     }
     int find(int x) {
         if (x == par[x]) return x;
-        par[x] = find(par[x]);
-        return par[x];
+        return par[x] = find(par[x]);
     }
     void merge(int x, int y) {
         int rx = find(x), ry = find(y);
