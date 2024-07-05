a977254f181c3c65d966f3b9f0455ad092317dee
Yueyuan Huang <harvyhyy@gmail.com>
2024-07-05 20:36:44 +0800

    Simplify manacher

--- string/manacher.cpp
+++ string/manacher.cpp
@@ -3,25 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> manacher_odd(string& s) {
-    int n = s.size();
-    s = "$" + s + "^";
+vector<int> manacher(const string& s) {
+    string t = "#";
+    for (auto c: s) t += c, t += '#';
+    int n = t.size();
+    t = "$" + t + "^";
     vector<int> p(n+2);
     for (int i = 1, l = 1, r = 1; i <= n; i++) {
         p[i] = max(0, min(r-i, p[l + (r-i)]));
-        while (s[i-p[i]] == s[i+p[i]])
-            ++p[i];
+        while (t[i-p[i]] == t[i+p[i]]) p[i]++;
         if (i+p[i] > r) l = i-p[i], r = i+p[i];
     }
-    return vector<int>(begin(p)+1, end(p)-1);
-}
-
-vector<int> manacher(const string& s) {
-    string t = "#";
-    for (auto c: s)
-        t += c, t += '#';
-    auto&& res = manacher_odd(t);
-    return vector<int>(begin(res)+1, end(res)-1);
+    return vector<int>(begin(p)+2, end(p)-2);
 }
 
 int main() {
