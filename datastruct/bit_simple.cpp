25dbc606f58bf8bb065bc25b15277887dc3e7f15
Yueyuan Huang <harvyhyy@gmail.com>
2024-07-15 00:45:23 +0800

    Modify bit_simple

--- datastruct/bit_simple.cpp
+++ datastruct/bit_simple.cpp
@@ -2,16 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <class T>
 struct BIT {
     int n;
-    vector<int> t;
+    vector<T> t;
 
     BIT(int _n): n(_n) { t.assign(n+1, 0); }
-    void add(int x, int v) {
+    void add(int x, T v) {
         while (x <= n) t[x] += v, x += x & -x;
     }
-    int query(int x) {
-        int r = 0;
+    T query(int x) {
+        T r = 0;
         while (x) r += t[x], x -= x & -x;
         return r;
     }
