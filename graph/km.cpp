99f97c944f74a0678d13e32ebc66003dcd5c2127
Yueyuan Huang <harvyhyy@gmail.com>
2024-07-15 14:48:56 +0800

    Update KM

--- graph/km.cpp
+++ graph/km.cpp
@@ -1,79 +1,65 @@
-/* 2023 SH Jan C : Bipartite Graph Pairing */
+// https://www.luogu.com.cn/problem/P6577
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
-struct Place {
-    int x, y;
-} p1[410], p2[410]; // target, current
-int n, m; // size of grid, number of vertices
-
-// KM algorithm: find an optimal pairing plan for a bipartite graph
-// optimal means maximizing the sum of weights
-const int INF = 1e9, MAXN = 500;     // MAXN := number of vertices of each side
-int weight[MAXN+7][MAXN+7]; // weights of edges between two sides
-int mb[MAXN+7], vb[MAXN+7], ka[MAXN+7], kb[MAXN+7], p[MAXN+7], c[MAXN+7];
-int qf, qb, q[MAXN+7];
-void bfs(int u) { // find the pairing
-    int a, v = 0, vl = 0, d;
-    for (int i = 1; i <= m; i++) p[i] = 0, c[i] = INF;
-    mb[v] = u;
-    do {
-        a = mb[v], d = INF, vb[v] = 1;
-        for (int b = 1; b <= m; b++)
-            if (!vb[b]) {
-                if (c[b] > ka[a]+kb[b]-weight[a][b])
-                    c[b] = ka[a]+kb[b]-weight[a][b], p[b] = v;
-                if (c[b] < d)
-                    d = c[b], vl = b;
-            }
-        for (int b = 0; b <= m; b++) {
-            if (vb[b]) ka[mb[b]] -= d, kb[b] += d;
-            else c[b] -= d;
-        }
-        v = vl;
-    } while (mb[v]);
-    while (v) mb[v] = mb[p[v]], v = p[v];
-}
-
-ll km() { // return the maximum weight sum
-    for (int i = 1; i <= m; i++)
-        mb[i] = ka[i] = kb[i] = 0;
-    for (int a = 1; a <= m; a++) {
-        for (int b = 1; b <= m; b++) vb[b] = 0;
-        bfs(a);
+class KM { // Hungarian
+  public:
+    KM(int _n): n(_n) { e.assign(n+1, vector<ll>(n+1, -inf)); }
+    void addedge(int u, int v, int w) { e[u][v] = w; }
+    ll km() { // do matching and return the optimal sum of matching
+        lx.assign(n+1, 0); ly.assign(n+1, 0);
+        couple.assign(n+1, -1);
+        for (int i = 1; i <= n; i++) match(i);
+        ll sum = 0;
+        for (int i = 1; i <= n; i++) sum += e[couple[i]][i];
+        return sum;
     }
-    ll res = 0;
-    for (int b = 1; b <= m; b++)
-        res += weight[mb[b]][b];
-    return res;
-}
+    vector<int> matching() { return {couple.begin()+1, couple.end()}; }
+  private:
+    // -inf should be small enough to prevent nonexistent edges from being chosen
+    const ll inf = 1e18;
+    int n; // #vertices in each set
+    vector<vector<ll>> e; // adjacent matrix
+    vector<ll> lx, ly; // weight on vertices  $l(u)+l(v) \le w(u, v)$
+    vector<int> couple; // couple[y] := optimal matching of y
+    void match(int u) {
+        vector<int> pre(n+1, 0);
+        vector<ll> slack(n+1, inf);
+        vector<bool> vis(n+1, 0); // vis[y]: if matching of y is found
+        int y = 0;
+        couple[y] = u;
+        do {
+            int ny = 0, x = couple[y];
+            ll md = inf;
+            vis[y] = 1;
+            for (int i = 1; i <= n; i++) {
+                if (vis[i]) continue;
+                ll d = lx[x]+ly[i] - e[x][i];
+                if (d < slack[i]) slack[i] = d, pre[i] = y;
+                if (slack[i] < md) md = slack[i], ny = i;
+            }
+            for (int i = 0; i <= n; i++) {
+                if (vis[i]) lx[couple[i]] -= md, ly[i] += md;
+                else slack[i] -= md;
+            }
+            y = ny;
+        } while (~couple[y]);
+        for (; y; y = pre[y]) couple[y] = couple[pre[y]];
+    }
+};
 
 int main() {
-    int T;
-    scanf("%d", &T);
-    while (T--) {
-        scanf("%d", &n);
-        getchar(); // '\n'
-        m = 0;
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= n; ++j)
-                if (getchar() == '#') p1[++m] = {i, j};
-            getchar();
-        }
-        m = 0;
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= n; ++j)
-                if (getchar() == '#') p2[++m] = {i, j};
-            getchar();
-        }
-        ++m;
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j < m; ++j)
-                weight[i][j] = -abs(p1[i].x-p2[j].x)-abs(p1[i].y-p2[j].y);
-            weight[i][m] = 0; // to Komorebi
-        }
-        printf("%lld\n", -km());
+    int n, m;
+    cin >> n >> m;
+    KM km(n);
+    while (m--) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        km.addedge(u, v, w);
     }
+    cout << km.km() << '\n';
+    for (auto e: km.matching()) cout << e << ' ';
+    cout << '\n';
     return 0;
 }
