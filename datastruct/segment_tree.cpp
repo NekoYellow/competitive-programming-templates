156b51f7fb6c1ba1c230cf00ca5d2c99fc497a69
Yueyuan Huang <harvyhyy@gmail.com>
2024-07-31 23:30:05 +0800

    Update segment tree

--- datastruct/segment_tree.cpp
+++ datastruct/segment_tree.cpp
@@ -53,6 +53,54 @@ struct SegTree {
     T query(int lo, int hi) { return query(0, 0, size / 2, lo, hi); }
 };
 
+
+namespace Simple { // simple segmentree: point modification & range query
+
+
+template<class T>
+class SegTree {
+    int n;
+    vector<T> t;
+    /* TO FILL IN */
+    const T e = 0; // identity of op
+    T op(T a, T b) { return max(a, b); }
+
+    void update(int pos, int pl, int pr, int i, T val) {
+        if (pl == pr) {
+            t[pos] = val;
+            return;
+        }
+        int m = (pl + pr)/2;
+        if (i <= m) update(pos*2, pl, m, i, val);
+        else update(pos*2+1, m+1, pr, i, val);
+        t[pos] = op(t[pos*2], t[pos*2+1]);
+    }
+    T query(int pos, int pl, int pr, int l, int r) {
+        if (l <= pl && pr <= r) {
+            return t[pos];
+        }
+        int m = (pl + pr)/2;
+        if (r <= m) return query(pos*2, pl, m, l, r);
+        if (r > m) return query(pos*2+1, m+1, pr, l, r);
+        return op(query(pos*2, pl, m, l, r), query(pos*2+1, m+1, pr, l, r));
+    }
+
+  public:
+    SegTree(int _n) : n(_n) {
+        t.assign(_n*4, e);
+    }
+    void update(int i, T val) {
+        return update(1, 0, n, i, val);
+    }
+    T query(int l, int r) {
+        return query(1, 0, n, l, r);
+    }
+};
+
+
+} // namespace Simple
+
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
