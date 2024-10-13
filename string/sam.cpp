5b1890fb1fb0d3078bdfb2b459df2f9f437ce139
Yueyuan Huang <harvyhyy@gmail.com>
2024-10-13 18:02:25 +0800

    Update SAM

--- string/sam.cpp
+++ string/sam.cpp
@@ -62,3 +62,62 @@ int main() {
 
     return 0;
 }
+
+
+namespace Raw {
+
+
+// https://loj.ac/p/2033
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+const int N = 1e5+7; // at most 2N-1 nodes / 3N-4 edges
+int sz, last;
+int len[N<<1], fa[N<<1], cnt[N<<1];
+unordered_map<int, int> nxt[N<<1];
+ll num; // number of unique substrings
+
+void init(int h) {
+    for (int i = 0; i < h; i++) {
+        len[i] = 0; fa[i] = -1; cnt[i] = 0;
+        nxt[i].clear();
+    }
+    sz = 1; last = 0; num = 0;
+}
+void extend(int c) {
+    int cur = sz++;
+    len[cur] = len[last]+1; fa[cur] = 0; cnt[cur] = 1;
+    int p = last; last = cur;
+    for (; p != -1 && !nxt[p][c]; p = fa[p]) nxt[p][c] = cur;
+    if (p != -1) {
+        int q = nxt[p][c];
+        if (len[p]+1 == len[q]) fa[cur] = q;
+        else {
+            int cp = sz++;
+            len[cp] = len[p]+1; fa[cp] = fa[q]; nxt[cp] = nxt[q];
+            for (; p != -1 && nxt[p][c] == q; p = fa[p]) nxt[p][c] = cp;
+            fa[q] = fa[cur] = cp;
+        }
+    }
+    num += len[cur] - len[fa[cur]];
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    init(N<<1);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int c;
+        cin >> c;
+        extend(c);
+        cout << num << '\n';
+    }
+
+    return 0;
+}
+
+
+}
